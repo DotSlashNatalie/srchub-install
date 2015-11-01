b65d1a6425f3aec84dfc5bc57d4de58553ee3e40
Nathan Adams <adamsna@datanethost.net>
2015-10-31 21:20:15 -0500

    updating install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apt-get install python-pip wget
+apt-get install -y --assume-yes python-pip wget
 pip install python2-pythondialog
 wget -qO- "https://srchub.org/p/srchub-install/source/file/master/srchub-install.py" | python
