bbc107d06f09343ef5503d750240bd09d2f272f3
Nathan Adams <adamsna@datanethost.net>
2015-11-01 09:00:16 -0600

    moving mariadb server to install last

--- srchub-install.py
+++ srchub-install.py
@@ -233,8 +233,8 @@ def install_packages():
     code = d.yesno("Do you want me to attempt to install the needed packages?")
     if code == d.OK:
         d.gauge_start("Installing...")
-        packages = ["git", "mercurial", "subversion", "mariadb-server", "mariadb-client", "libapache2-mod-php5",
-                        "php5-curl", "php5-mysql", "php5-cli", "git-daemon-run", "gitweb", "php-pear", "patch"]
+        packages = ["git", "mercurial", "subversion", "mariadb-client", "libapache2-mod-php5",
+                        "php5-curl", "php5-mysql", "php5-cli", "git-daemon-run", "gitweb", "php-pear", "patch", "mariadb-server"]
         percent = 0
         i = 0.0
         for package in packages:
